1adf142d362ee5a4c33574408d6d73bfdb9b496e
Junxiang Chen <chenfrey920417@outlook.com>
2021-02-20 16:32:57 +0000

    Init configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-unused-components": "warn",
+    "no-unused-vars": "warn",
+  },
+};
