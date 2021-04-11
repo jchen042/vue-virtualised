10c5479f29aa74e0fbab0d8d9ef2c24dfe5e33b6
Junxiang Chen <chenfrey920417@outlook.com>
2021-04-11 01:40:11 +0100

    Temp change: TO BE REVISIT

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import Vue, { ComponentOptions, Plugin } from "vue";
+import type { DefineComponent, Plugin } from "vue";
 
 import VirtualisedList from "@/components/VirtualisedList.vue";
 import VirtualisedTree from "@/components/VirtualisedTree.vue";
 
 // Define typescript interfaces for installable component
-type InstallableComponent = ComponentOptions<typeof Vue> & Plugin;
+type InstallableComponent = DefineComponent & Plugin;
 
 function install(): void {
   [VirtualisedList, VirtualisedTree].forEach((component) => {
@@ -18,6 +18,37 @@ function install(): void {
   });
 }
 
-install();
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+// https://stackoverflow.com/a/56458070/10890476
+declare global {
+  interface Window {
+    Vue: DefineComponent;
+  }
+  // https://stackoverflow.com/a/42304473/10890476
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      document: Document;
+      window: Window;
+      navigator: Navigator;
+      Vue: DefineComponent;
+    }
+  }
+}
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
 
 export { VirtualisedList, VirtualisedTree };
