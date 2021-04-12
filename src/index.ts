9d5aa8ed98594591feb032fc54ed82973adc6a92
Junxiang Chen <chenfrey920417@outlook.com>
2021-04-12 23:24:13 +0100

    Vue 3 adjustment for build entry file

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,15 @@
-import type { DefineComponent, Plugin } from "vue";
+import type { App } from "vue";
 
 import VirtualisedList from "@/components/VirtualisedList.vue";
 import VirtualisedTree from "@/components/VirtualisedTree.vue";
 
-// Define typescript interfaces for installable component
-type InstallableComponent = DefineComponent & Plugin;
+const components = [VirtualisedList, VirtualisedTree];
 
-function install(): void {
-  [VirtualisedList, VirtualisedTree].forEach((component) => {
-    // Assign InstallableComponent type
-    const installable = (component as unknown) as InstallableComponent;
-
-    // Attach install function executed by Vue.use()
-    installable.install = (Vue) => {
-      Vue.component(component.name, installable);
-    };
-  });
-}
-
-// Create module definition for Vue.use()
-const plugin = {
-  install,
+// Vue 3 doesn't have Vue.use() anymore. Instead, it has instance method app.use().
+const install = (app: App): void => {
+  components.forEach((component) => app.component(component.name, component));
 };
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
-// https://stackoverflow.com/a/56458070/10890476
-declare global {
-  interface Window {
-    Vue: DefineComponent;
-  }
-  // https://stackoverflow.com/a/42304473/10890476
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    interface Global {
-      document: Document;
-      window: Window;
-      navigator: Navigator;
-      Vue: DefineComponent;
-    }
-  }
-}
-
-let GlobalVue = null;
-if (typeof window !== "undefined") {
-  GlobalVue = window.Vue;
-} else if (typeof global !== "undefined") {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
+export default { install };
 
 export { VirtualisedList, VirtualisedTree };
