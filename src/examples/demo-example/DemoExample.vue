ccd9413201cc96e47ae14564ad5945778e72e35c
Junxiang Chen <chenfrey920417@outlook.com>
2021-05-08 17:50:04 +0100

    Complete all mutual methods

--- src/examples/demo-example/DemoExample.vue
+++ src/examples/demo-example/DemoExample.vue
@@ -6,12 +6,9 @@
         <button @click="scrollListToStart">Scroll To Start</button>
         <button @click="scrollListToEnd">Scroll To End</button>
         <button @click="scrollListToIndex">Scroll To Index</button>
-        <input
-          type="number"
-          min="0"
-          :modelValue="listIndex"
-          :value="listIndex"
-        />
+        <input v-model="listIndex" type="number" min="0" />
+        <button @click="scrollListToHeight">Scroll To Height</button>
+        <input v-model="listHeight" type="number" min="0" />
       </div>
       <virtualised-list
         ref="listView"
@@ -32,6 +29,14 @@
     </div>
     <div class="tree-container">
       <div class="title">VirtualisedTree</div>
+      <div>
+        <button @click="scrollTreeToStart">Scroll To Start</button>
+        <button @click="scrollTreeToEnd">Scroll To End</button>
+        <button @click="scrollTreeToIndex">Scroll To Index</button>
+        <input v-model="treeIndex" type="number" min="0" />
+        <button @click="scrollTreeToHeight">Scroll To Height</button>
+        <input v-model="treeHeight" type="number" min="0" />
+      </div>
       <virtualised-tree
         ref="treeView"
         :nodes="nodes"
@@ -76,13 +81,14 @@ export default defineComponent({
     const treeView = ref<typeof VirtualisedTree | null>(null);
 
     const listIndex = ref<number>(0);
+    const listHeight = ref<number>(0);
 
     const scrollListToStart = () => listView.value?.scrollToStart();
     const scrollListToEnd = () => listView.value?.scrollToEnd();
-    const scrollListToIndex = () => {
-      // console.log(listIndex);
-      // listView.value?.scrollToIndex(listIndex.value);
-    };
+    const scrollListToIndex = () =>
+      listView.value?.scrollToIndex(listIndex.value);
+    const scrollListToHeight = () =>
+      listView.value?.scrollToHeight(listHeight.value);
 
     const nodes = constructFixedTree(6, 15, 5);
 
@@ -92,6 +98,16 @@ export default defineComponent({
     const listCellRenderer = (node: NodeModel) => [
       h("div", { class: "cell-container list-cell-container" }, node.label),
     ];
+
+    const treeIndex = ref<number>(0);
+    const treeHeight = ref<number>(0);
+
+    const scrollTreeToStart = () => treeView.value?.scrollToStart();
+    const scrollTreeToEnd = () => treeView.value?.scrollToEnd();
+    const scrollTreeToIndex = () =>
+      treeView.value?.scrollToIndex(treeIndex.value);
+    const scrollTreeToHeight = () =>
+      treeView.value?.scrollToHeight(treeHeight.value);
 
     const treeCellRenderer = (node: NodeModel, index: number) => [
       h(
@@ -200,13 +216,21 @@ export default defineComponent({
       listView,
       treeView,
       listIndex,
+      listHeight,
       scrollListToStart,
       scrollListToEnd,
       scrollListToIndex,
+      scrollListToHeight,
       nodes,
       onChange,
       listCellRenderer,
       treeCellRenderer,
+      treeIndex,
+      treeHeight,
+      scrollTreeToStart,
+      scrollTreeToEnd,
+      scrollTreeToIndex,
+      scrollTreeToHeight,
       handleScroll,
       handleStartReached,
       handleEndReached,
